12ba901935c459f417ba75d46050a327de35f3f5
Paul Caron <paul.a.caron@gmail.com>
2020-10-01 09:28:53 -0500

    update

--- chip8.cpp
+++ chip8.cpp
@@ -5,6 +5,7 @@
 
 using namespace std;
 
+
 uint8_t fontset[80] =
 {
     0xF0, 0x90, 0x90, 0x90, 0xF0, //0
@@ -25,6 +26,25 @@ uint8_t fontset[80] =
     0xF0, 0x80, 0xF0, 0x80, 0x80  //F
 };
 
+uint8_t super_fontset[160]={
+    0xff, 0xff, 0xc3, 0xc3, 0xc3, 0xc3, 0xc3, 0xc3, 0xff, 0xff, // 0
+    0x18, 0x78, 0x78, 0x18, 0x18, 0x18, 0x18, 0x18, 0xff, 0xff, // 1
+    0xff, 0xff, 0x03, 0x03, 0xff, 0xff, 0xc0, 0xc0, 0xff, 0xff, // 2
+    0xff, 0xff, 0x03, 0x03, 0xff, 0xff, 0x03, 0x03, 0xff, 0xff, // 3
+    0xc3, 0xc3, 0xc3, 0xc3, 0xff, 0xff, 0x03, 0x03, 0x03, 0x03, // 4
+    0xff, 0xff, 0xc0, 0xc0, 0xff, 0xff, 0x03, 0x03, 0xff, 0xff, // 5
+    0xff, 0xff, 0xc0, 0xc0, 0xff, 0xff, 0xc3, 0xc3, 0xff, 0xff, // 6
+    0xff, 0xff, 0x03, 0x03, 0x06, 0x0c, 0x18, 0x18, 0x18, 0x18, // 7
+    0xff, 0xff, 0xc3, 0xc3, 0xff, 0xff, 0xc3, 0xc3, 0xff, 0xff, // 8
+    0xff, 0xff, 0xc3, 0xc3, 0xff, 0xff, 0x03, 0x03, 0xff, 0xff, // 9
+    0x7e, 0xff, 0xc3, 0xc3, 0xc3, 0xff, 0xff, 0xc3, 0xc3, 0xc3, // A
+    0xfc, 0xfc, 0xc3, 0xc3, 0xfc, 0xfc, 0xc3, 0xc3, 0xfc, 0xfc, // B
+    0x3c, 0xff, 0xc3, 0xc0, 0xc0, 0xc0, 0xc0, 0xc3, 0xff, 0x3c, // C
+    0xfc, 0xfe, 0xc3, 0xc3, 0xc3, 0xc3, 0xc3, 0xc3, 0xfe, 0xfc, // D
+    0xff, 0xff, 0xc0, 0xc0, 0xff, 0xff, 0xc0, 0xc0, 0xff, 0xff, // E
+    0xff, 0xff, 0xc0, 0xc0, 0xff, 0xff, 0xc0, 0xc0, 0xc0, 0xc0
+};
+
 Chip8::Chip8(){
     sound_timer = 0;
     delay_timer = 0;
@@ -33,6 +53,7 @@ Chip8::Chip8(){
     pc = 512;
     sp = 0;
     opcode = 0;
+    extended_mode = false;
     for(int i=0;i<16;++i){
         stack.at(i) = 0;
         V.at(i) = 0;
@@ -41,6 +62,8 @@ Chip8::Chip8(){
     for(auto& pixel: gfx) pixel=0;
     for(auto& b: memory) b=0;
     for(int i=0;i<80;++i) memory.at(i) = fontset[i];
+    for(int i=0;i<160;++i) memory.at(i+80) = super_fontset[i];
+    for(auto& r:Rpl) r=0;
     srand(time(0));
 }
 
@@ -72,18 +95,71 @@ uint8_t y(uint16_t opcode){
 }
 
 void Chip8::drawSprite(){
-    uint8_t Vx = (V[x(opcode)]+256)%64;
-    uint8_t Vy = (V[y(opcode)]+256)%32;
+    uint8_t Vx = (V[x(opcode)]+256)%(64*(1+extended_mode));
+    uint8_t Vy = (V[y(opcode)]+256)%(32*(1+extended_mode));
     uint8_t height = n(opcode);
+    if((!height) && extended_mode) height=16;
     uint8_t spriteRowData;
     V[0xF] = 0;
+    if(!extended_mode){
     for(int row=0;row<height;++row){
         spriteRowData=memory[I+row];
         for(int col=0;col<8;++col)
             if(spriteRowData&(128>>col))
                 if(!(gfx.at(64*((Vy+row)%32)+((Vx+col)%64))^=1)) V[0xF]=1;//collision detection
     }
+    }else{
+    if(height!=16){
+    for(int row=0;row<height;++row){
+        spriteRowData=memory[I+row];
+        for(int col=0;col<8;++col)
+            if(spriteRowData&(128>>col))
+                if(!(gfx.at(128*((Vy+row)%64)+((Vx+col)%128))^=1)) V[0xF]=1;//collision detection
+    }
+    }else{
+    for(int row=0;row<height;++row){
+        spriteRowData=memory[I+row*2];
+        for(int col=0;col<8;++col)
+            if(spriteRowData&(128>>col))
+                if(!(gfx.at(128*((Vy+row)%64)+((Vx+col)%128))^=1))
+                V[0xF]=1;//collisio
+    }
+    Vx+=8;
+    for(int row=0;row<height;++row){
+        spriteRowData=memory[1+I+row*2];
+        for(int col=0;col<8;++col)
+            if(spriteRowData&(128>>col))
+               if(!(gfx.at(128*((Vy+row)%64)+((Vx+col)%128))^=1))
+               V[0xF]=1;
+    }
+    }}
     pc+=2;
+}
+
+void Chip8::down(){
+    uint8_t N = n(opcode);
+    for(int i=8191;i>=128*N;--i)
+        gfx[i]=gfx[i-128*N];
+    for(int i=0;i<128*N;++i)
+        gfx[i]=0;
+}
+
+void Chip8::right(){
+    for(int row=0;row<64;++row)
+    for(int col=127;col>4;col--)
+        gfx[row*128+col]=gfx[row*128+col-4];
+    for(int row=0;row<64;++row)
+    for(int col=0;col<4;col++)
+        gfx[row*128+col]=0;
+}
+
+void Chip8::left(){
+    for(int row=0;row<64;++row)
+    for(int col=0;col<128-4;++col)
+        gfx[row*128+col]=gfx[row*128+col+4];
+    for(int row=0;row<64;++row)
+    for(int col=124;col<128;++col)
+        gfx[row*128+col]=0;
 }
 
 void Chip8::cycle() {
@@ -95,7 +171,23 @@ void Chip8::cycle() {
                                     break;
                          case 0xEE: pc = stack.at(--sp)+ 2;
                                     break;
-                         default: exit(1);
+                         case 0xFB: right();
+                                    pc += 2;
+                                    break;
+                         case 0xFC: left();
+                                    pc += 2;
+                                    break;
+                         case 0xFD: exit(0); pc += 2; break;
+                         case 0xFE: extended_mode = 0;
+                                    pc += 2;
+                                    break;
+                         case 0xFF: extended_mode = 1;
+                                    pc += 2;
+                                    break;
+                         default: if(y(opcode)==0xC){
+                                      down();
+                                      pc += 2;
+                                  }else exit(1);
                      }
                      break;
         case 0x1000: pc = nnn(opcode);  break;
@@ -132,7 +224,8 @@ void Chip8::cycle() {
                                       break;
                          case 0x0004: V[x(opcode)] += V[y(opcode)];
                                       if(V[y(opcode)] > (0xFF - V[x(opcode)])) V[0xF] = 1;
-                                      else V[0xF] = 0;                                                                                                                                                                 pc += 2;
+                                      else V[0xF] = 0;
+                                      pc += 2;
                                       break;
                          case 0x0005: if(V[y(opcode)] > V[x(opcode)]) V[0xF] = 0;
                                       else V[0xF] = 1;
@@ -152,7 +245,8 @@ void Chip8::cycle() {
                                       break;
                          case 0x000E: V[0xF] = V[x(opcode)] >> 7;
                                       V[x(opcode)] <<= 1;
-                                      pc += 2;                                                                                                                                                                         break;
+                                      pc += 2;
+                                      break;
                          default: exit(1);
                      }
                      break;
@@ -164,7 +258,8 @@ void Chip8::cycle() {
                      break;
         case 0xB000: pc = nnn(opcode) + V[0];
                      break;
-        case 0xC000: V[x(opcode)] = (rand() % (256)) & nn(opcode);                                                                                                                                    pc += 2;
+        case 0xC000: V[x(opcode)] = (rand() % (256)) & nn(opcode);
+                     pc += 2;
                      break;
         case 0xD000: drawSprite();
                      break;
@@ -206,6 +301,10 @@ void Chip8::cycle() {
                          case 0x0029: I = V[x(opcode)] * 5;
                                       pc += 2;
                                       break;
+                         //point I to 10byte font sprite for Vx
+                         case 0x0030: I = 80 + V[x(opcode)]*10;
+                                      pc += 2;
+                                      break;
                          case 0x0033: memory.at(I) = V[x(opcode)] / 100;
                                       memory.at(I + 1) = (V[x(opcode)] / 10) % 10;
                                       memory.at(I + 2) = V[x(opcode)] % 10;
@@ -222,13 +321,23 @@ void Chip8::cycle() {
                                       I += x(opcode) + 1;
                                       pc += 2;
                                       break;
+                         //Store V0..VX in RPL user flags (X <= 7)
+                         case 0x0075: for(int i=0;i<x(opcode);++i)
+                                          Rpl.at(i) = V[i];
+                                      pc += 2;
+                                      break;
+                         //Read V0..VX from RPL user flags (X <= 7) 
+                         case 0x0085: for(int i=0;i<x(opcode);++i)
+                                          V[i] = Rpl.at(i);
+                                      pc += 2;
+                                      break;
                          default: exit(1);
                      }
                      break;
         default: exit(1);
     }
 
-    if(instruction_count == 9){
+    if(instruction_count == 9*(1+extended_mode)){
         if (delay_timer > 0) --delay_timer;
         if (sound_timer > 0) --sound_timer;
         instruction_count =0;
--- chip8.hpp
+++ chip8.hpp
@@ -1,25 +1,30 @@
 #include <cstdint>
-#include <string>
 #include <array>
+
 
 class Chip8{
 public:
-    std::array<uint8_t,2048> gfx;
+    std::array<uint8_t,8192> gfx;
     std::array<uint8_t,16> V;
     std::array<uint8_t,16> key;
     std::array<uint16_t,16> stack;
+    std::array<uint8_t,8> Rpl;
     uint16_t sp;
     uint16_t opcode;
     uint16_t pc;
+    uint16_t I;
     uint8_t delay_timer;
     uint8_t sound_timer;
-    uint16_t I;
+    bool extended_mode;
     bool load(char * path);
     void cycle();
     Chip8();
 private:
     int instruction_count=0; //slows timers
     void drawSprite();
+    void right();
+    void left();
+    void down();
     std::array<uint8_t,4096> memory;
 };
 
